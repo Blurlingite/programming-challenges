7878314b09ca5ffa8b41377e38967713c0562835
Vishnu Dawah <vkdawah@gmail.com>
2020-05-03 16:43:07 -0400

    Add queens-attack-2

---
+++ Javascript/queens-attack-2.js
@@ -0,0 +1,13 @@
+// https://www.hackerrank.com/challenges/queens-attack-2/problem
+
+let n = 5; // 5x5 board
+let k = 3;
+
+let r_q = 4; // queen row
+let c_q = 3; // queen column
+
+let obstacles = [
+  [5, 5],
+  [4, 2],
+  [2, 3],
+];
